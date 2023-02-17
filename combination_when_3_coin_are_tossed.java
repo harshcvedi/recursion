38a7d6fc07d22b195293bc80e8bc351cdd2f6659
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2023-02-17 20:05:06 +0530

    Create combination_when_3_coin_are_tossed.java

---
+++ combination_when_3_coin_are_tossed.java
@@ -0,0 +1,16 @@
+public static void main(String args[]) {
+		int n=3;
+		print(n, "");
+
+
+ 
+	}
+	public static void print(int n,String ans){
+		if(n==0){
+			System.out.print(ans+" ");
+			return;
+		}
+
+		print(n-1, ans+"H");
+		print(n-1, ans+"T");
+	}
